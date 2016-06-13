47ad76f8bec61461ac0838daabcb726f035262aa
axhello <zhanglin-pc@live.com>
2016-06-13 20:06:39 +0800

    update code

--- assets/js/vpalyer.js
+++ assets/js/vpalyer.js
@@ -4,24 +4,24 @@ var vm = new Vue({
         return {
             audio: document.getElementsByTagName('audio')[0],
             storage: window.localStorage,
-            range: 0.5,
+            range: 0,
             progress: 0,
             playingTitle: 'Hero',
-            playingArtist: [{ name: 'SmK' }],
+            playingArtist: 'SmK',
+            picUrl: 'http://p3.music.126.net/iZOGOeVEHz0fmEV4qpUjow==/1421668538040772.jpg',
             showCurrentTime: '0:00',
             showDurationTime: '0:00',
             currentIndex: 0,
             index: 0,
             search: '',
-            picUrl: 'http://p3.music.126.net/iZOGOeVEHz0fmEV4qpUjow==/1421668538040772.jpg',
-            lists: [],
-            playList: [],
-            mlist: [],
-            next: false,
+            songLists: [],
+            historyLists: [],
+            playingLists: [],
             pagenumber: 1,
             pages: 0,
             offset: 30,
-            isnext: false,
+            next: false,
+            isNext: false,
             lock: false,
             listOpen: false,
             isMuted: false,
@@ -29,10 +29,13 @@ var vm = new Vue({
         }
     },
     ready: function() {
-        this.audio.volume = 0.3;
+        this.audio.volume = 0.5;
         setInterval(this.setProgress, 500);
-        this.mlist = JSON.parse(this.storage.getItem('testObject'));
         this.audio.addEventListener("ended", this.autoNextPlay);
+        if (this.storage.getItem('listObject') === null) { 
+            this.storage.setItem('listObject', '[]'); 
+        }
+         this.playingLists = JSON.parse(this.storage.getItem('listObject'));
     },
     methods: {
         setAutoPlay: function() {
@@ -66,10 +69,6 @@ var vm = new Vue({
         isListOpen: function() {
             this.listOpen = !this.listOpen
         },
-        whoActive: function () {
-            this.isActive.player = !this.isActive.player;
-            this.isActive.search = !this.isActive.search
-        },
         setProgress: function() {
             var currentTime = this.audio.currentTime;
             MM = parseInt(currentTime / 60);
@@ -93,8 +92,8 @@ var vm = new Vue({
             this.$http.get('api/searchApi.php', {
                 's': this.search
             }).then(function(data) {
-                this.lists = data.data.result.songs;
-                this.isnext = true;
+                this.songLists = data.data.result.songs;
+                this.isNext = true;
             }, function(response) {
                 // error callback
             });
@@ -105,22 +104,34 @@ var vm = new Vue({
                 'id': id
             }).then(function(data) {
                 var result = data.data.songs[0];
-                var mdata = {
-                    'id': result.id,
-                    'title': result.name,
-                    'url': result.mp3Url,
-                    'picUrl': result.album.picUrl,
-                    'artists': result.artists
+                var id = result.id, 
+                title = result.name, 
+                url = result.mp3Url, 
+                picUrl = result.album.picUrl, 
+                artists;
+                if (result.artists.length === 1) {
+                    artists = result.artists[0].name;
+                } else if(result.artists.length === 2) {
+                    artists = result.artists[0].name +'/'+ result.artists[1].name
+                } else if (result.artists.length === 3) {
+                    artists = result.artists[0].name +'/'+ result.artists[1].name + '/'+ result.artists[2].name;
                 };
-                this.playingTitle = result.name;
-                this.playingArtist = result.artists;
-                this.picUrl = result.album.picUrl;
-                this.audio.src = mdata.url;
+                var mdata = {
+                    'id': id,
+                    'title': title,
+                    'url': url,
+                    'picUrl': picUrl,
+                    'artists': artists
+                };
+                this.playingTitle = title;
+                this.playingArtist = artists;
+                this.picUrl = picUrl;
+                this.audio.src = url;
                 this.audio.play();
-                var obj = JSON.parse(this.storage.getItem('testObject'));
-                if (this.storage.getItem('testObject') === null) {
-                    this.playList.push(mdata);
-                    this.storage.setItem('testObject', JSON.stringify(this.playList));
+                var obj = JSON.parse(this.storage.getItem('listObject'));
+                if (obj === null) {
+                    this.playingLists.push(mdata);
+                    this.storage.setItem('listObject', JSON.stringify(this.playingLists));
                 } else {
                     for (var i = 0; i < obj.length; i++) {
                         if (mdata.url === obj[i].url) {
@@ -131,8 +142,9 @@ var vm = new Vue({
                         }
                     };
                     if (this.lock == false) {
-                        this.playList.push(mdata);
-                        this.storage.setItem('testObject', JSON.stringify(this.playList));
+                        this.playingLists = obj;
+                        this.playingLists.push(mdata);
+                        this.storage.setItem('listObject', JSON.stringify(this.playingLists));
                     } else {
                         alert('歌曲已经存在歌单');
                     };
@@ -142,18 +154,25 @@ var vm = new Vue({
             });
         },
         playHistoryList: function(id, index) {
-            this.isPlay = true;
+            this.isPlay = false;
             this.$http.get('api/detailApi.php', {
                 'id': id
             }).then(function(data) {
                 var music = data.data.songs[0];
                 this.audio.src = music.mp3Url;
-                this.audio.play();
+                setTimeout(this.setPlay, 1500);
+                var artists;
+                if (music.artists.length === 1) {
+                    artists = music.artists[0].name;
+                } else if(music.artists.length === 2) {
+                    artists = music.artists[0].name +'/'+ music.artists[1].name
+                } else if (music.artists.length === 3) {
+                    artists = music.artists[0].name +'/'+ music.artists[1].name + '/'+ music.artists[2].name;
+                };
                 this.playingTitle = music.name;
-                this.playingArtist = music.artists;
+                this.playingArtist = artists;
                 this.picUrl = music.album.picUrl;
                 this.currentIndex = index;
-                console.log(this.currentIndex);
             }, function(response) {
                 // error callback
             });
@@ -165,7 +184,7 @@ var vm = new Vue({
                 's': this.search,
                 'p': this.pages
             }).then(function(data) {
-                this.lists = data.data.result.songs;
+                this.songLists = data.data.result.songs;
             }, function(response) {
                 // error callback
             });
@@ -178,14 +197,14 @@ var vm = new Vue({
                 's': this.search,
                 'p': this.pages
             }).then(function(data) {
-                this.lists = data.data.result.songs;
+                this.songLists = data.data.result.songs;
             }, function(response) {
                 // error callback
             });
         },
         autoNextPlay: function() {
             this.isPlay = false;
-            var obj = JSON.parse(this.storage.getItem('testObject'));
+            var obj = JSON.parse(this.storage.getItem('listObject'));
             this.index = ++this.currentIndex;
             if (this.index == obj.length) {
                 this.currentIndex = 0;
@@ -202,11 +221,9 @@ var vm = new Vue({
             }
         },
         clickProgress: function(event) {
-            var a = this.audio;
-            var z = a.buffered.end(a.buffered.length-1);
             var target = event.target;
-            console.log(target.offsetWidth - this.progress);
-            console.log(z);
+            console.log(target.clientWidth);
+            console.log(target.offsetWidth);
             // setInterval(function() {
             //     drag.style.left = (audio.currentTime / audio.duration) * (window.innerWidth - 30) + "px";
             //     speed.style.left = -((window.innerWidth) - (audio.currentTime / audio.duration) * (window.innerWidth - 30)) + "px";
