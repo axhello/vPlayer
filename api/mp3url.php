02cdfa9dc20cad22aea8e44fdb1d22648dee91d9
axhello <zhanglin-pc@live.com>
2017-01-01 14:50:11 +0800

    优化样式；压缩css和js

--- api/mp3url.php
+++ api/mp3url.php
@@ -8,4 +8,6 @@ $song_id = $_GET['id'];
 if (!empty($song_id)) {
   $mp3url = $api->mp3url($song_id);
   print_r($mp3url);
+} else {
+	return;
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,35 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    fontmin = require('gulp-fontmin');
+ 
+//压缩css
+gulp.task('minifycss', function() {
+    return gulp.src('src/css/*.css') //需要操作的文件
+        .pipe(rename({ suffix: '.min' })) //rename压缩后的文件名
+        .pipe(minifycss()) //执行压缩
+        .pipe(gulp.dest('dist/css')); //输出文件夹
+});
+//压缩js
+gulp.task('scripts', function() {
+    return gulp.src(['src/js/vue.js', 'src/js/vue-resource.min.js', 'src/js/vplayer.js'])
+        .pipe(concat('app.js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
 
-gulp.task('scripts',function(){
-    return gulp.src(["script/vue.min.js","script/vue-resource.min.js","script/vplayer.js"])
-    .pipe(concat('app.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+//压缩图标字体
+gulp.task('fontmin', function () {
+    return gulp.src('src/fonts/*.ttf')
+        .pipe(fontmin())
+        .pipe(gulp.dest('dist/fonts'));
+});
+
+//gulp watch
+gulp.task('watch', function () {  
+   gulp.watch('src/css/*.css', ['minifycss']);  
+   gulp.watch('src/js/vplayer.js', ['scripts']);  
 });
